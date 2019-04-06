7238e35e5fa94efa92a658e980f1f086f3c08944
Roberta Melo <you@example.com>
2019-04-06 13:07:55 -0300

    Criação de estrutura do projeto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './src/pages/login/login.component';
+import { ListComponent } from './pages/list/list.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { EditComponent } from './pages/edit/edit.component';
+import { HeaderComponent } from './template/header.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { RegisterModelComponent } from './template/register-model/register-model.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ListComponent,
+    RegisterComponent,
+    EditComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegisterModelComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/register-model/register-model.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register-model',
+  templateUrl: './register-model.component.html',
+  styleUrls: ['./register-model.component.css']
+})
+export class RegisterModelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
